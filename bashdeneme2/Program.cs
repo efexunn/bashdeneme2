da4b35cab868903f72c66ffd34fc74e631c6ba69
dev global <bozkurtefe64@gmail.com>
2022-12-07 15:02:53 +0300

    yeni branch ile push

--- bashdeneme2/Program.cs
+++ bashdeneme2/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using bashdeneme2;
+
 Console.WriteLine("bash2!");
 Console.WriteLine("dejnfjskdn");
+
+var Obje = new Deneme()
+{
+    sayi1 = 133,
+    sayi2 = 123
+};
+
+var result = Obje.SumofNums(Obje.sayi1, Obje.sayi2);
+
+Console.WriteLine($"toplam = {result}"); 
